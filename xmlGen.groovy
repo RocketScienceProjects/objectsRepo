07eccbb5cfe80e45f1d0b26f5bd6c59c8d9e742d
okram999 <nirish.okram@gmail.com>
2017-03-19 19:42:08 -0500

    Added the new file for the XML gen

--- xmlGen.groovy
+++ xmlGen.groovy
@@ -1,27 +1,40 @@
+
 import groovy.xml.*
 
+//declare number of workflows to Loop
+//def wf_count = $wf_count
+
+def workflows = [[ name: 'A' , file: 'fileA' , [srcFolder1: "TgtFolder1", srcFolder2: "TgtFolder2"], objectName: 'wf_A' , objectType: 'workflow', sourceRepository: 'DEV2'],
+[ name: 'B' , file: 'fileB' , [srcFolder4: "TgtFolder4", srcFolder3: "TgtFolder3"], objectName: 'wf_B' , objectType: 'workflow', sourceRepository: 'DEV2']]
+
+
+
+
 StreamingMarkupBuilder builder = new StreamingMarkupBuilder()
-        builder.encoding = 'UTF-8'
-        String filterSoapMessage = builder.bind {
-            mkp.xmlDeclaration()
-            'udm.DeploymentPackage'(version:'$BUILD_NUMBER', application: "informaticaApp"){
-             deployables {
-               'powercenter.PowercenterXml'(name:"/wf_multifolder", file:"wf_multifolder.XML"){
-                 scanPlaceholders{ mkp.yield(true) }
-                 sourceRepository{ mkp.yield("DEV2") }
-                 folderNameMap{ mkp.yield("this will a map") }
-                 'objectNames' {
-                   value { mkp.yield('wf_multifolder.XML') }
-                 }
-                 'objectTypes' {
-                   value {  mkp.yield('workflow') }
-                 }
-                }
-             }
-
-            dependencyResolution{ mkp.yield('LATEST') }
-            undeployDependencies{ mkp.yield(false) }
-            }
+builder.encoding = 'UTF-8'
+String filterSoapMessage = builder.bind {
+  mkp.xmlDeclaration()
+  'udm.DeploymentPackage'(version:'$BUILD_NUMBER', application: "informaticaApp"){
+    deployables {
+      workflows.each { item ->
+      'powercenter.PowercenterXml'(name:"item.name", file:"item.file"){
+        scanPlaceholders{ mkp.yield(true) }
+        sourceRepository{ mkp.yield("item.sourceRepository") }
+        'folderNameMap' {
+          entry( key:'multifolder', "{{multifolderTST}}" )
         }
+        'objectNames' {
+          value { mkp.yield('item.objectName') }
+        }
+        'objectTypes' {
+          value {  mkp.yield('item.objectType') }
+        }
+      }
+    }
+  }
+    dependencyResolution{ mkp.yield('LATEST') }
+    undeployDependencies{ mkp.yield(false) }
+  }
+}
 
- println XmlUtil.serialize(filterSoapMessage)
+println XmlUtil.serialize(filterSoapMessage)
