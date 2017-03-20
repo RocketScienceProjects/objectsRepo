71c218c16e3f8ee7b80d5d4697260896cf140152
okram999 <nirish.okram@gmail.com>
2017-03-20 16:31:24 -0500

    testing with the method

---
+++ release1/generateXML.groovy
@@ -0,0 +1,48 @@
+import groovy.xml.*
+
+def GenerateXML(String file) {
+  def workflows = [
+  [ name: 'A', file: 'fileA', objectName: 'wf_A', objectType: 'workflow', sourceRepository: 'DEV2', folderNames: [ multifolder: '{{multifolderTST}}', multifolder2: '{{multifolderTST2}}' ]],
+
+  [ name: 'B',
+  file: 'fileB',
+  objectName: 'wf_B',
+  objectType: 'workflow',
+  sourceRepository: 'DEV2',
+  folderNames: [ multifolder3: '{{multifolderTST3}}',
+  multifolder4: '{{multifolderTST4}}' ]]
+  ]
+
+  def writer = new FileWriter(file)
+  def builder = new StreamingMarkupBuilder()
+  builder.encoding = 'UTF-8'
+  writer << builder.bind {
+    mkp.xmlDeclaration()
+    mkp.declareNamespace(udm :'http://www.w3.org/2001/XMLSchema')
+    mkp.declareNamespace(powercenter:'http://www.w3.org/2001/XMLSchema')
+    delegate.udm.DeploymentPackage(version:'$BUILD_NUMBER', application: "informaticaApp"){
+      delegate.deployables {
+        workflows.each { item ->
+          delegate.powercenter.PowercenterXml(name:item.name, file:item.file) {
+            delegate.scanPlaceholders(true)
+            delegate.sourceRepository(item.sourceRepository)
+            delegate.folderNameMap {
+              item.folderNames.each { name, value ->
+                it.entry(key:name, value)
+              }
+            }
+            delegate.objectNames {
+              delegate.value(item.objectName)
+            }
+            delegate.objectTypes {
+              delegate.value(item.objectType)
+            }
+          }
+        }
+      }
+      delegate.dependencyResolution('LATEST')
+      delegate.undeployDependencies(false)
+    }
+  }
+  return file;
+}
